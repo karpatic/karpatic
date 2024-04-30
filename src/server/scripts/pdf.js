34c47c8b4d5e0270a23899529ffaf20e798bf61f
Charles Karpati <charles.karpati@gmail.com>
2024-04-30 13:21:01 -0400

    fix server endpoint return

--- src/server/scripts/pdf.js
+++ src/server/scripts/pdf.js
@@ -1,10 +1,14 @@
 const { execSync } = require("child_process"); 
+const path = require("path");
+
 
 async function generate_pdf(req, res) {
   const { latex } = req.body;
   if (!latex) return false;  
   execSync(`pdflatex -interaction=nonstopmode ./content.tex`, { stdio: "inherit" });
-  return res.download("./content.pdf");
+  const fileName = './content.pdf'
+  if (!fileName) return res.status(500).send("PDF generation failed");
+  return res.sendFile(fileName, err => err && console.error(err) && res.status(500).end())
 }
 
 module.exports = { generate_pdf };
